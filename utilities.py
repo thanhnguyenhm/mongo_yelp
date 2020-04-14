e1922036b03c0dd4c46e04e1db57e8c855fc7c19
anis1996 <anisdhapa11@gmail.com>
2020-04-14 14:59:13 -0700

    Checking User Name

--- utilities.py
+++ utilities.py
@@ -6,6 +6,7 @@ def random_rating_id(length=22):
     st = string.ascii_lowercase + string.digits + string.ascii_uppercase
     return ''.join(random.sample(st, length))
 
+
 # Need to decide if we should use static variable with fix value or generate user id for every session
 # def random_user_id(length=22):
 #     st = string.ascii_lowercase + string.digits + string.ascii_uppercase
