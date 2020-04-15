956a8539e5e47ccd2877676fe32e8053d1892911
anis1996 <anisdhapa11@gmail.com>
2020-04-15 13:43:23 -0700

    remove extra files

--- User_Functions.py
+++ User_Functions.py
@@ -18,7 +18,7 @@ def find_business_based_on_name_partial():
     """
     Find business based on partial name
     """
-    while(True):
+    while (True):
         print()
         business_name = input(
             'Please enter partial business name or type "back" or "quit": ')
@@ -39,7 +39,8 @@ def find_business_based_on_name_partial():
             print('Address: ' + business_object['address'])
             print('City: ' + business_object['city'])
             print('State: ' + business_object['state'])
-            print('Average Rating: ' + str(business_object['stars']) + ' Review Count: ' + str(business_object['review_count']))
+            print('Average Rating: ' + str(business_object['stars']) + ' Review Count: ' + str(
+                business_object['review_count']))
             print('categories: ' + str(business_object['categories']))
             print('#############################')
 
@@ -48,7 +49,7 @@ def find_business_based_on_name_full():
     """
     Find business based on full name
     """
-    while(True):
+    while (True):
         print()
         business_name = input(
             'Please enter full business name or type "back" or "quit": ')
@@ -76,7 +77,7 @@ def give_business_rating():
     """
     Give a rating to the business
     """
-    while(True):
+    while (True):
         print()
         business_name = input(
             'Please enter full business name to give Ratings or type "back" or "quit": ')
@@ -111,15 +112,15 @@ def give_business_rating():
             return
 
         print()
-        review_col.insert({ "review_id": utilities.random_rating_id(),
-                                "user_id": app.USER_ID,
-                                "business_id": business_object['business_id'],
-                                "stars": business_rating,
-                                "date": datetime.datetime.now().strftime("%Y-%m-%d"),
-                                "text": comment,
-                                "useful": 0,
-                                "funny": 0,
-                                "cool": 0})
+        review_col.insert({"review_id": utilities.random_rating_id(),
+                           "user_id": app.USER_ID,
+                           "business_id": business_object['business_id'],
+                           "stars": business_rating,
+                           "date": datetime.datetime.today(),
+                           "text": comment,
+                           "useful": 0,
+                           "funny": 0,
+                           "cool": 0})
         print('Thank you for providing your rating for ' + business_object['name'])
 
 
@@ -127,7 +128,7 @@ def delete_business_rating():
     """
     Delete rating to the business from userID
     """
-    while(True):
+    while (True):
         print()
         business_name = input(
             'Please enter full business name to delete Ratings or type "back" or "quit": ')
@@ -147,7 +148,6 @@ def delete_business_rating():
         business_id = business_object['business_id']
         review_obj = review_col.find_one({"user_id": app.USER_ID})
 
-        
         if review_obj:
             print('This is your review for ' + business_object['name'] + ': ')
             print('Stars: ' + str(review_obj['stars']))
@@ -168,7 +168,6 @@ def delete_business_rating():
         print()
 
 
-<<<<<<< HEAD
 """
 {
     // string, 22 character business id, maps to business in business.json
@@ -176,20 +175,13 @@ def delete_business_rating():
 
     // string which is a comma-separated list of timestamps for each checkin, each with format YYYY-MM-DD HH:MM:SS
     "date": "2016-04-26 19:49:16, 2016-08-30 18:36:57, 2016-10-15 02:45:18, 2016-11-18 01:54:50, 2017-04-20 18:39:06, 2017-05-03 17:58:02"
-<<<<<<< HEAD
 }
 Note: You need to convert string with comma separated to Array to support this query
 """
-=======
->>>>>>> bec923731dde0c41e02284288073c7bc3416a246
-||||||| 571142d
-}"""
-=======
-Note: You need to convert string with comma separated to Array to support this query
-"""
->>>>>>> merge_branch
+
+
 def checkin():
-    while(True):
+    while (True):
         print()
         business_name = input(
             'Please enter full business name to check-in or type "back" or '
@@ -208,7 +200,7 @@ def checkin():
         postal_code_list = []
         for bus in business_object:
             postal_code_list.append(bus['postal_code'])
-            print(f'Business name: {bus["name"]}, City: {bus["city"]}, Postal Code: {bus["postal_code"]}')
+            print(f'Business name: {bus["name"]}, City: {bus["city"]}, Postal Code: 'f'{bus["postal_code"]}')
 
         # input postal code to choose which business to check in
         # same business_name can have multiple locations
@@ -217,28 +209,11 @@ def checkin():
             postal_code = input('Postal code or "back": ')
             if postal_code == 'back':
                 return
-        date = datetime.datetime.now()
         store = business_col.find_one({"$text": {"$search": business_name}, "postal_code": postal_code})
+
         print()
-<<<<<<< HEAD
-<<<<<<< HEAD
-        checkin_col.update({"business_id": store['business_id']},
-                           {"$push": {"date": datetime.datetime.today()}})
-=======
-        # Update required version of date time
-        checkin_col.insert({"business_id": store['business_id'], "date": date.strftime("%Y-%m-%d %H:%M:%S")})
-
->>>>>>> bec923731dde0c41e02284288073c7bc3416a246
-||||||| 571142d
-        checkin_col.insert({"business_id": store['business_id'],
-                                "date": datetime.datetime.today()})
-=======
         date = datetime.datetime.now()
-
         checkin_col.update({"business_id": store['business_id']},
                            {"$push": {"date": date.strftime("%Y-%m-%d %H:%M:%S")}})
-
->>>>>>> merge_branch
         print(f'Thank you for checking in at {store["name"]}-{postal_code}')
     print()
-
